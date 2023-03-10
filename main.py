64e49a42bd1ba9503c00523355cadccaeea1f016
Brianchib <chibuikeokafor861@gmail.com>
2023-03-10 21:58:08 +0000

    add login ui

---
+++ main.py
@@ -0,0 +1,18 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+import sys
+
+from ui.login_ui import Ui_Form
+
+if __name__ == "__main__":
+        app = QtWidgets.QApplication(sys.argv)
+        Form = QtWidgets.QWidget()
+        ui = Ui_Form()
+        ui.setupUi(Form)
+        Form.show()
+        sys.exit(app.exec_())
+

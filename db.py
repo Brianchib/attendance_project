bf759be96889fb6bdda06edc229be4230743184d
Brianchib <chibuikeokafor861@gmail.com>
2023-04-13 01:01:17 +0000

    update db

--- db.py
+++ db.py
@@ -1,22 +1,17 @@
 from PyQt5 import QtSql
+import sqlite3
 
-db = QtSql.QSqlDatabase.addDatabase("QSQLITE")
-db.setDatabaseName("attendance_db.sqlite")
-
-if not db.open():
-    print("Database connection Error")
-
-user_data_query = QtSql.QSqlQuery()
-user_data_query.exec_("create table userdata (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(100) NOT NULL, password VARCHAR(100) NOT NULL);")
-user_data_query.exec_("insert into userdata (username, password) values ('admin', 'admin');")
-user_data_query.exec_("select * from userdata")
-user_data_query.first()
-print(user_data_query.value("username"), user_data_query.value("password"))
+con = sqlite3.connect("attendance_db.sqlite")
+cur = con.cursor()
 
 
-create_student_query = QtSql.QSqlQuery()
-create_student_query.exec_("create table student (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(100) NOT NULL, index_number INTEGER NOT NULL);")
+create_user_data_table = "create table userdata (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, username VARCHAR(100) NOT NULL, password VARCHAR(100) NOT NULL);"
+insert_user_data = "insert into userdata (username, password) values ('admin', 'admin');"
+create_student_table = "create table student (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(100) NOT NULL, index_number VARCHAR(100) NOT NULL);"
+create_attendance_table = "create table attendance (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(100) NOT NULL, index_number VARCHAR(100) NOT NULL, time DATETIME NOT NULL, present VARCHAR(10), course_code VARCHAR(10), course_date DATE );"
 
-
-create_attendance_table = QtSql.QSqlQuery()
-create_attendance_table.exec_("create table attendance (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name VARCHAR(100) NOT NULL, index_number INTEGER NOT NULL, time DATETIME NOT NULL, present BOOLEAN, course_code VARCHAR(10), course_date DATE );")
+cur.execute(create_user_data_table)
+cur.execute(insert_user_data)
+cur.execute(create_student_table)
+cur.execute(create_attendance_table)
+con.commit()

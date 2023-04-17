acebad9f48b41f3b9ab561dc98b719908a6f9812
Brianchib <chibuikeokafor861@gmail.com>
2023-04-17 20:37:51 +0000

    refactor codebase

--- src/add_new_student.py
+++ src/add_new_student.py
@@ -1,9 +1,7 @@
 import json
 import sqlite3
-
 import cv2
 import numpy as np
-
 from dsClass.align_custom import AlignCustom
 from dsClass.face_feature import FaceFeature
 from dsClass.mtcnn_detect import MTCNNDetect
@@ -13,52 +11,32 @@ from dsClass.tf_graph import FaceRecGraph
 def add_new_student(student_name, student_index_number):
     con = sqlite3.connect("../attendance_db.sqlite")
     cur = con.cursor()
-    FRGraph = FaceRecGraph()
+    face_rec_graph = FaceRecGraph()
     aligner = AlignCustom()
-    extract_feature = FaceFeature(FRGraph)
-    face_detect = MTCNNDetect(FRGraph, scale_factor=2)
-    vs = cv2.VideoCapture(0)
-    # get input from webcam
-
-    f = open("../dsClass/facerec_128D.txt", "r")
-    data_set = json.loads(f.read())
-    person_imgs = {"Left": [], "Right": [], "Center": []}
-    person_features = {"Left": [], "Right": [], "Center": []}
-    print(
-        "Please start turning slowly. Press 'q' to save and add this new user to the dataset"
-    )
+    extracted_face_feature = FaceFeature(face_rec_graph)
+    face_detect = MTCNNDetect(face_rec_graph, scale_factor=2)
+    video_from_camera = cv2.VideoCapture(0)
+    face_rec_file = open("../dsClass/facerec_128D.txt", "r")
+    data_from_face_rec_file = json.loads(face_rec_file.read())
+    student_images = {"Left": [], "Right": [], "Center": []}
+    student_face_features = {"Left": [], "Right": [], "Center": []}
     while True:
-        _, frame = vs.read()
+        _, frame = video_from_camera.read()
         rects, landmarks = face_detect.detect_face(frame, 80)
-        # min face size is set to 80x80
         for i, rect in enumerate(rects):
             aligned_frame, pos = aligner.align(160, frame, landmarks[i])
             if len(aligned_frame) == 160 and len(aligned_frame[0]) == 160:
-                person_imgs[pos].append(aligned_frame)
-                cv2.imshow("Pres 'q' to save and close window", aligned_frame)
+                student_images[pos].append(aligned_frame)
+                cv2.imshow("Recording Face, Pres 'q' to save and close window", aligned_frame)
         key = cv2.waitKey(1) & 0xFF
         if key == 30 or key == ord("q"):
             break
 
-    # changes to be done here. write to db instead of excell file.
-
-    cur.execute(
-        "INSERT INTO student(name, index_number) VALUES(?, ?)",
-        (student_name, student_index_number),
-    )
+    cur.execute("INSERT INTO student(name, index_number) VALUES(?, ?)",(student_name, student_index_number),)
     con.commit()
-    for (
-        pos
-    ) in (
-        person_imgs
-    ):  # there r some exceptions here, but I'll just leave it as this to keep it simple
-        person_features[pos] = [
-            np.mean(extract_feature.get_features(person_imgs[pos]), axis=0).tolist()
-        ]
-    data_set[student_name] = person_features
-    f = open("../dsClass/facerec_128D.txt", "w")
-    f.write(json.dumps(data_set))
+    for pos in student_images:
+        student_face_features[pos] = [np.mean(extracted_face_feature.get_features(student_images[pos]), axis=0).tolist()]
+    data_from_face_rec_file[student_name] = student_face_features
+    face_rec_file = open("../dsClass/facerec_128D.txt", "w")
+    face_rec_file.write(json.dumps(data_from_face_rec_file))
     return True
-
-
-# add_new_student("Andea", 123443)
--- src/take_attendance.py
+++ src/take_attendance.py
@@ -2,10 +2,8 @@ import datetime
 import json
 import sqlite3
 import sys
-
 import cv2
 import numpy as np
-
 from dsClass.align_custom import AlignCustom
 from dsClass.face_feature import FaceFeature
 from dsClass.mtcnn_detect import MTCNNDetect
@@ -13,144 +11,72 @@ from dsClass.tf_graph import FaceRecGraph
 
 
 def take_attendance(course_code):
-    FRGraph = FaceRecGraph()
+    face_rec_graph = FaceRecGraph()
     aligner = AlignCustom()
-    extract_feature = FaceFeature(FRGraph)
-    face_detect = MTCNNDetect(FRGraph, scale_factor=2)
+    extracted_face_feature = FaceFeature(face_rec_graph)
+    face_detect = MTCNNDetect(face_rec_graph, scale_factor=2)
     con = sqlite3.connect("../attendance_db.sqlite")
     cur = con.cursor()
     todays_date = datetime.date.today()
     currentDate = datetime.datetime.utcnow()
-
-    all_student_names = dict(
-        cur.execute("SELECT name, index_number FROM student").fetchall()
-    )
-
-    print("[INFO] camera sensor warming up...")
-    vs = cv2.VideoCapture(0)
-    # get input from webcam
+    all_student_names = dict(cur.execute("SELECT name, index_number FROM student").fetchall())
+    video_from_camera = cv2.VideoCapture(0)
     while True:
-        _, frame = vs.read()
-        # u can certainly add a roi here but for the sake of a demo i'll just leave it as simple as this
+        _, frame = video_from_camera.read()
         rects, landmarks = face_detect.detect_face(frame, 80)
-        # min face size is set to 80x80
         aligns = []
         positions = []
-
         for i, rect in enumerate(rects):
             aligned_face, face_pos = aligner.align(160, frame, landmarks[i])
             if len(aligned_face) == 160 and len(aligned_face[0]) == 160:
                 aligns.append(aligned_face)
                 positions.append(face_pos)
-            else:
-                print("Align face failed")  # log
         if len(aligns) > 0:
-            features_arr = extract_feature.get_features(aligns)
-            recog_data = findPeople(features_arr, positions)
+            features_arr = extracted_face_feature.get_features(aligns)
+            recog_data = recognize_face(features_arr, positions)
             for i, rect in enumerate(rects):
-                cv2.rectangle(
-                    frame,
-                    (rect[0], rect[1]),
-                    (rect[0] + rect[2], rect[1] + rect[3]),
-                    (0, 255, 0),
-                    2,
-                )  # draw bounding box for the face
-                cv2.putText(
-                    frame,
-                    recog_data[i][0] + " - " + str(recog_data[i][1]) + "%",
-                    (rect[0], rect[1]),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    1,
-                    (255, 255, 255),
-                    1,
-                    cv2.LINE_AA,
-                )
+                cv2.rectangle(frame, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 2, )
+                cv2.putText(frame, recog_data[i][0] + " - " + str(recog_data[i][1]) + "%", (rect[0], rect[1]), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 1, cv2.LINE_AA,)
                 for name, index_number in all_student_names.items():
                     if name == recog_data[i][0]:
-                        present_student_res = cur.execute(
-                            "SELECT name FROM attendance"
-                        ).fetchall()
-                        present_student_names = [
-                            each for (each,) in present_student_res
-                        ]
+                        present_student_res = cur.execute("SELECT name FROM attendance").fetchall()
+                        present_student_names = [each for (each,) in present_student_res]
                         if name not in present_student_names:
-                            cur.execute(
-                                "INSERT INTO attendance (name, index_number, time, present, course_code, course_date) VALUES (?, ?, ?, TRUE, ?,?)",
-                                (
-                                    name,
-                                    index_number,
-                                    currentDate,
-                                    course_code,
-                                    todays_date,
-                                ),
-                            )
+                            cur.execute("INSERT INTO attendance (name, index_number, time, present, course_code, course_date) VALUES (?, ?, ?, TRUE, ?,?)", (name,index_number,currentDate,course_code, todays_date,),)
                             con.commit()
                         else:
-                            student_courses = cur.execute(
-                                "SELECT course_code, course_date FROM attendance WHERE name = ?",
-                                (name,),
-                            ).fetchall()
+                            student_courses = cur.execute("SELECT course_code, course_date FROM attendance WHERE name = ?",(name,),).fetchall()
                             course_tuple = (course_code, str(todays_date))
                             if course_tuple not in student_courses:
-                                cur.execute(
-                                    "INSERT INTO attendance (name, index_number, time, present, course_code, course_date) VALUES (?, ?, ?, TRUE, ?,?)",
-                                    (
-                                        name,
-                                        index_number,
-                                        currentDate,
-                                        course_code,
-                                        todays_date,
-                                    ),
-                                )
+                                cur.execute("INSERT INTO attendance (name, index_number, time, present, course_code, course_date) VALUES (?, ?, ?, TRUE, ?,?)",(name,index_number,currentDate,course_code,todays_date,),)
                                 con.commit()
 
-        cv2.imshow("Taking Attendance", frame)
+        cv2.imshow("Taking Attendance, Pres 'q' to save and close window", frame)
         key = cv2.waitKey(1) & 0xFF
         if key == 27 or key == ord("q"):
             break
-    vs.release()  # camera release
+    video_from_camera.release()
     cv2.destroyAllWindows()
 
 
-"""
-facerec_128D.txt Data Structure:
-{
-"Person ID": {
-    "Center": [[128D vector]],
-    "Left": [[128D vector]],
-    "Right": [[128D Vector]]
-    }
-}
-This function basically does a simple linear search for 
-^the 128D vector with the min distance to the 128D vector of the face on screen
-"""
-
-
-def findPeople(features_arr, positions, thres=0.6, percent_thres=70):
-    """
-    :param features_arr: a list of 128d Features of all faces on screen
-    :param positions: a list of face position types of all faces on screen
-    :param thres: distance threshold
-    :return: person name and percentage
-    """
-    f = open("../dsClass/facerec_128D.txt", "r")
-    data_set = json.loads(f.read())
-    returnRes = []
-    for i, features_128D in enumerate(features_arr):
-        result = "Unknown"
+# RecognizeFace function performs a linear search for 128D vector on the screen to that of the file
+def recognize_face(face_feature_from_screen, positions, threshold=0.6, percent_threshold=70):
+    face_rec_file = open("../dsClass/facerec_128D.txt", "r")
+    data_from_face_rec_file = json.loads(face_rec_file.read())
+    student_name_percentage = []
+    for i, face_feature in enumerate(face_feature_from_screen):
+        student_result = "Unregistered Student"
         smallest = sys.maxsize
-        for person in data_set.keys():
-            person_data = data_set[person][positions[i]]
-            for data in person_data:
-                distance = np.sqrt(np.sum(np.square(data - features_128D)))
+        for student in data_from_face_rec_file.keys():
+            student_data = data_from_face_rec_file[student][positions[i]]
+            for data in student_data:
+                distance = np.sqrt(np.sum(np.square(data - face_feature)))
                 if distance < smallest:
                     smallest = distance
-                    result = person
-        percentage = min(100, 100 * thres / smallest)
-        if percentage <= percent_thres:
-            result = "Unknown"
-        returnRes.append((result, percentage))
-    return returnRes
+                    student_result = student
+        percentage = min(100, 100 * threshold / smallest)
+        if percentage <= percent_threshold:
+            student_result = "Unregistered Student"
+        student_name_percentage.append((student_result, percentage))
+    return student_name_percentage
 
-
-# take_attendance("CSIT123")
